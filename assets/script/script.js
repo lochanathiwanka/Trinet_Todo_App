ad7f2cf9b9c223650e89522c88252865a426cb65
lochanathiwanka <lochanathiwanka62@gmail.com>
2021-07-26 16:23:57 +0530

    Save task & get all tasks from the db.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,7 @@
+$(document).ready(() => {
+    getAllTasks();
+})
+
 //add new task
 $('#btnAdd').click(function () {
     //get date
@@ -22,55 +26,68 @@ $('#btnAdd').click(function () {
             "title": topic,
             "body": description,
             "finished": false
+        });
+    }
+});
+
+//save task
+function saveTask(dataObj) {
+    (async () => {
+        fetch('https://trinet-todo-app.herokuapp.com/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(dataObj),
         })
-            .then(response => {
-                console.log(response);
+            .then(response => response.json())
+            .then(data => {
+                getAllTasks();
             })
             .catch(error => {
                 console.error(error);
             });
-    }
-});
-
-/*$('#container').append(
-                            `
-                <div class="task" id="task-0">
-                <span class="id" id="id-0">12354</span>
-                <div class="header" id="header-0">
-                    <div class="color-circle"></div>
-                    <h3 class="task-topic" id="task-topic-0">Study</h3>
-                    <p class="topic-description" id="topic-description-0">
-                        I have to learn 3<br>subjects for my A/L’s.
-                    </p>
-                </div>
-                <span class="task-time" id="task-time-0">08.00 am</span>
-                <span class="task-date" id="task-date-0">2021-07-20</span>
-                <span class="update-task material-icons-outlined" id="update-task-0">refresh</span>
-                <span class="delete-task material-icons-outlined" id="delete-task-0">delete</span>
-            </div>
-            `
-                        );*/
-
-//save task
-async function saveTask(dataObj) {
-    await fetch('https://trinet-todo-app.herokuapp.com/create', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(dataObj),
-    })
-        .then(response => response.json())
-        .then(data => data)
-        .catch(error => {
-            console.error(error);
-        });
+    })();
 }
 
 //get all tasks
-fetch('https://trinet-todo-app.herokuapp.com/get-all-by-user', {
-    mode: "no-cors"
-})
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+function getAllTasks() {
+    (async () => {
+        await fetch('https://trinet-todo-app.herokuapp.com/get-all-tasks', {
+            method: 'GET',
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                let taskList = data;
+                for (let i = 0; i < taskList.length; i++) {
+                    let date = moment(taskList[i].date).format('YYYY-MM-DD');
+
+                    $('#container').append(
+                        `
+                        <div class="task" id=task${0}>
+                            <span class="id" id=id-${i}>${taskList[i]._id}</span>
+                            <div class="header" id=header-${i}>
+                                <div class="color-circle"></div>
+                                <h3 class="task-topic" id=task-topic-${i}>${taskList[i].title}</h3>
+                                <p class="topic-description" id=topic-description-${i}>
+                                    ${taskList[i].body}
+                                </p>
+                            </div>
+                            <span class="task-time" id=task-time-${i}>${taskList[i].time}</span>
+                            <span class="task-date" id=task-date-${i}>${date}</span>
+                            <span class="update-task material-icons-outlined" id=update-task-${i}>refresh</span>
+                            <span class="delete-task material-icons-outlined" id=delete-task-${i}>delete</span>
+                        </div>
+                        `
+                    );
+                }
+
+            })
+            .catch(error => console.error(error));
+    })()
+}
 
